3ecf7739281b7fca1ba3e50b0a6ce43f628bbf55
Manuel Guerra <manu.guerra2010@gmail.com>
2021-08-18 22:16:32 -0300

    first challenge

---
+++ clase1.js
@@ -0,0 +1,21 @@
+var nombre = "Manuel";
+
+var apellido = "Guerra";
+
+var nombreCompleto = "Manuel Guerra";
+
+const currentYear = 2021;
+
+var textoIngresado = prompt("Ingrese nombre:");
+
+console.log("Hola: ",textoIngresado,"!");
+
+var bienvenida = "Bienvenido a la clase"+" "+textoIngresado;
+
+alert(bienvenida);
+
+var year= prompt("Ingrese año de nacimiento: ");
+
+var edad = currentYear - parseInt(year);
+
+alert(edad);
