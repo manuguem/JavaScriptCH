d9ad6089175dc0648a25a704bbbcca277b861bcc
Manuel Guerra <manu.guerra2010@gmail.com>
2021-09-10 20:57:08 -0300

    Upload first final project

--- js/carrito.js
+++ js/carrito.js
@@ -2,11 +2,11 @@
 /* Array Barra de busqueda de productos */
 
 const Productos = [{ id: 1, producto: "Fuente modular 500w EVGA", precio: 20500, watts: "500w", marcas: "EVGA" },
-{ id: 2, producto: "Fuente 800w AIGO", precio: 21800, watts: "800w", marcas: "Aigo" },
-{ id: 3, producto: "Fuente modular 550w EVGA", precio: 21999, watts: "550w", marcas: "EVGA" },
-{ id: 4, producto: "Fuente Akyga 600w", precio: 15990, watts: "600w", marcas: "Akyga" },
-{ id: 5, producto: "Fuente MPB 750w", precio: 18800, watts: "750w", marcas: "Mpb" },
-{ id: 6, producto: "Fuente Akyga 500w", precio: 8300, watts: "500w", marcas: "Akyga" }]
+{ producto: "Fuente 800w AIGO", precio: 21800, watts: "800w", marcas: "Aigo" },
+{ producto: "Fuente modular 550w EVGA", precio: 21999, watts: "550w", marcas: "EVGA" },
+{ producto: "Fuente Akyga 600w", precio: 15990, watts: "600w", marcas: "Akyga" },
+{ producto: "Fuente MPB 750w", precio: 18800, watts: "750w", marcas: "Mpb" },
+{ producto: "Fuente Akyga 500w", precio: 8300, watts: "500w", marcas: "Akyga" }]
 
 console.log("Fuente mejor calidad precio: ")
 const Fuente500EVGA = Productos.find(producto => producto.id === 1);
@@ -29,7 +29,7 @@ const Marca = Productos.filter(producto => producto.marcas === "Akyga");
 console.log(Marca);
 
 
-/* Ordenar Array de precios fuentes */
+/* Ordenar Array de precios productos */
 
 console.log("Ordenar productos por menor precio: ");
 const Menor_precio = [20500, 21800, 18800, 15990, 21999, 8300];
@@ -39,75 +39,37 @@ console.log("Ordenar productos por mayor precio: ")
 const Mayor_precio = [20500, 21800, 18800, 15990, 21999, 8300];
 console.log(Mayor_precio.sort((a, b) => b - a));
 
-/* Ingreso de fuentes a comprar y precio final */
+class Hardwarepart {
+    constructor(name) {
+        this.name = name.toLowerCase()
 
-// class Fuentepoder {
-//     constructor(nombre, precio, watts) {
-//         this.nombre = nombre;
-//         this.precio = precio;
-//         this.watts = watts;
-//     }
-
-//     sumaIva() {
-//         this.precio = this.precio * 2.1;
-//     }
-
-// }
-
-// let nombre = "";
-
-// do {
-
-//     nombre = prompt("Ingrese nombre del producto que desea comprar o 'salir' para culminar:");
-
-//     if (nombre == "fuente modular 500w evga") {
-
-//         const Fuentepoder1 = new Fuentepoder(nombre, 20500, "500watts");
-//         console.log(Fuentepoder1)
-//         alert("El precio del producto es: " + 20500)
-//     }
-
-//     else if (nombre == "fuente 800w aigo") {
-
-//         const Fuentepoder2 = new Fuentepoder(nombre, 21800, "800watts");
-//         console.log(Fuentepoder2)
-//         alert("El precio del producto es: " + 21800)
-
-//     }
-
-//     else if (nombre == "fuente modular 550w evga") {
-
-//         const Fuentepoder3 = new Fuentepoder(nombre, 21999, "550watts");
-//         console.log(Fuentepoder3)
-//         alert("El precio del producto es: " + 21999)
-
-//     }
-
-//     else if (nombre == "fuente akyga 600w") {
-
-//         const Fuentepoder4 = new Fuentepoder(nombre, 15990, "600watts");
-//         console.log(Fuentepoder4)
-
-//     }
-
-//     else if (nombre == "fuente mpb 750w") {
-
-//         const Fuentepoder5 = new Fuentepoder(nombre, 18800, "750watts");
-//         console.log(Fuentepoder5)
-
-//     }
-
-//     else if (nombre == "fuente 500w akyga") {
-
-//         const Fuentepoder6 = new Fuentepoder(nombre, 8300, "500watts");
-//         console.log(Fuentepoder6)
-
-//     }
-
-//     else {
-//         console.log("Producto no válido")
-//     }
+    }
+}
 
 
-// } while (nombre != "salir")
+/* Carrito de compras */
 
+const shoppingCartL = sessionStorage.getItem("shoppingC");
+const shoppingCart = [];
+
+if (shoppingCartL != null) {
+
+    const ShoppingLObject = JSON.parse(shoppingCartL);
+    for (const prod of ShoppingLObject) {
+        shoppingCart.push(new Hardwarepart(prod.name));
+    }
+}
+
+do {
+    let answer = prompt(`Hay ${shoppingCart.length} productos en el carrito. Presione "s" para seguir "c" para culminar`).toLowerCase()
+    if (answer != "s".toLowerCase()) break;
+
+    HardwarepartName = prompt(`Ingresar nombre producto: `)
+    const ProductName = new Hardwarepart(HardwarepartName);
+    shoppingCart.push(ProductName);
+
+    sessionStorage.setItem("ShoppingC", JSON.stringify(shoppingCart));
+
+} while (true)
+
+alert(`Compra finalizada. Usted agregó ${shoppingCart.length} productos al carrito`);
