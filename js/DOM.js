8a4114b70450327a50a4e6368f01248bb16755bc
Manuel Guerra <manu.guerra2010@gmail.com>
2021-09-19 23:14:01 -0300

    Update styles, js and features.html

--- js/DOM.js
+++ js/DOM.js
@@ -1,20 +1,44 @@
 /*DOM*/
 
-// const padre = document.getElementById('dad');
-// const dad = ["<p class='khulaYellow'>Rango de precio</p>", "$6000 a $10000", "$10000 a $20000", "$20000 a $50000", "$50000 a $100000"];
-// for (const rango of dad) {
-//     const p = document.createElement('p');
-//     p.innerHTML = rango
-//     padre.appendChild(p);
-// }
+const padre = document.getElementById('dad');
+const dad = ["<p class='khulaYellow'>Rango de precio</p>", "$6000 a $10000", "$10000 a $20000", "$20000 a $50000", "$50000 a $100000"];
+for (const rango of dad) {
+    const p = document.createElement('p');
+    p.innerHTML = rango
+    padre.appendChild(p);
+}
 
-let contadorCarrito = 0;
+/* Event contador carrito */
 
-let carrito = document.getElementById("btn-carrito");
+let contadorMSI = 0;
 
-carrito.addEventListener("click", respCarrito);
+let msi = document.getElementById("btn-msi");
 
-function respCarrito () {
-    contadorCarrito += 1;
-    carrito.innerHTML = "Agregar a carrito: " +contadorCarrito;
+msi.addEventListener("click", respMsi);
+
+function respMsi() {
+    contadorMSI += 1;
+    msi.innerHTML = "Agregar a carrito: " + contadorMSI;
+}
+
+let contadorRedragon = 0;
+
+let redragon = document.getElementById("btn-redragon");
+
+redragon.addEventListener("click", respRedragon);
+
+function respRedragon() {
+    contadorRedragon += 1;
+    redragon.innerHTML = "Agregar a carrito: " + contadorRedragon;
+}
+
+let contadorAsus = 0;
+
+let asus = document.getElementById("btn-asus");
+
+asus.addEventListener("click", respAsus);
+
+function respAsus() {
+    contadorAsus += 1;
+    asus.innerHTML = "Agregar a carrito: " + contadorAsus;
 }
